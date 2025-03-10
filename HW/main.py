616a8b0d7359cba1ea1098633889a69d04950a2a
kitetsuw <teamstylex900@gamil.com>
2025-03-10 18:03:57 +0300

    hw3

---
+++ HW/main.py
@@ -0,0 +1,39 @@
+'''import my_module as md
+from my_module import add, subtrakt
+print(md.add(3, 2))
+print(subtrakt(7, 2))
+'''
+import random
+from abc import ABC, abstractmethod
+
+class Hero(ABC):
+    def __init__(self, name, health, power):
+        self.name = name
+        self.health = health
+        self.power = power
+        self.__random_int = random.randint(1, 3)
+
+    def attack(self):
+        print(f"{self.name} атакует с силой {self.power}!")
+
+    def protection(self):
+        print(f"{self.name} защищается!")
+
+    def rest(self):
+        self.health += 10
+        print(f"{self.name} отдыхает и восстанавливает здоровье до {self.health} HP!")
+
+    def get_random_int(self):
+        return self.__random_int
+
+    @abstractmethod
+    def unique_attack(self):
+        pass
+
+    @abstractmethod
+    def unique_scream(self):
+        pass
+
+    @abstractmethod
+    def action(self):
+        pass
--- Lessons/my_package/main.py
+++
@@ -1,4 +0,0 @@
-import my_module as md
-from my_module import add, subtrakt
-print(md.add(3, 2))
-print(subtrakt(7, 2))
